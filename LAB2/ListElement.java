92c6a715c2d15c000b306fd14a7dc7680af8ab67
Jonathan Vargas <jonathanbvargas@hotmail.com>
2019-10-02 19:05:19 -0600

    Implemented addElement()
    Added code to implement addElement()

--- LAB2/ListElement.java
+++ LAB2/ListElement.java
@@ -14,3 +14,34 @@ public class ListElement {
 /**
 * @param data
 * an integer to be stored in the node
+*@return
+*	None
+**/
+public void setData(int data) {
+	this.data = data;
+}
+
+/**
+* @param 
+* 	None
+* @return
+*	None
+**/
+public int getData() {
+	return this.data;
+}
+
+/**
+* @param le
+* an integer to be stored in the node
+*@return
+*	None
+**/
+public void addElement(ListElement le)
+{
+	this.next = le;
+	le.previous = this;
+}
+
+
+}
